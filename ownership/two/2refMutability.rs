e7d302d68a48eca6cc2e500e1509bea172391138
mansi0xc <mansi.tibrewal76@gmail.com>
2025-08-28 22:27:53 +0530

    gitgit

---
+++ ownership/two/2refMutability.rs
@@ -0,0 +1,22 @@
+/*
+fn main() {
+    let s = String::from("hello");
+
+    change(&s);
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", world");
+}
+ */
+
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
---
+++ ownership/two/3existence.rs
@@ -0,0 +1,17 @@
+/*
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{r1}, {r2}");
+ */
+fn main() {
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+}
