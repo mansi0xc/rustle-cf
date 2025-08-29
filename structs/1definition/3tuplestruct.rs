c32f17a21291c93080fb0f64a9ebcdd9cb6bec9f
mansi0xc <mansi.tibrewal76@gmail.com>
2025-08-30 01:01:29 +0530

    meowwwww

--- structs/1definition/3tuplestruct.rs
+++ structs/1definition/3tuplestruct.rs
@@ -4,4 +4,10 @@ struct Point(i32, i32, i32);
 fn main() {
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
+
+    /*
+    Unlike tuples, tuple structs require you to name the type of the 
+    struct when you destructure them. For example, we would write let 
+    Point(x, y, z) = origin; to destructure the values in the origin 
+    point into variables named x, y, and z. */
 }
