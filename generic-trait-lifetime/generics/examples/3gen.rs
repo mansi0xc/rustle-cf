d5f3288f2caae09ef80e1e327004f9f1f2d06d94
mansi0xc <mansi.tibrewal76@gmail.com>
2025-09-12 00:16:40 +0530

    forgot to add examples

---
+++ generic-trait-lifetime/generics/examples/3gen.rs
@@ -0,0 +1,20 @@
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn main() {
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
+}
+
+// Equivalent to :
+/*
+let integer = Some(5);
+let float = Some(5.0);
+ */
